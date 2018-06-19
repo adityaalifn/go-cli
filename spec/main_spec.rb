bc7614363024ca9bc7d4a91e077a30d4a5504eb1
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-06-19 22:08:22 +0700

    trying TDD for non parameters running application

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,12 @@
-require_relative main
+require_relative '../lib/application'
 
-RSpec.describe Main do
+RSpec.describe Application do
   describe "#initialize" do
     it "running go-cli without any parameters" do
-      expect
+      app = Application.new
+      expect(Driver.TOTAL).to eq(5)
+      expect(Map.LENGTH).to eq(20)
+      expect(Map.WIDTH).to eq(20)
     end
     it "running go-cli and passing three parameters" do
 
