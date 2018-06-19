4f55fd22c65de34cd24d624e8d2d0cef85b0dd8e
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-06-19 07:57:19 +0700

    add test for running application

--- test/main_spec.rb
+++ test/main_spec.rb
@@ -2,8 +2,14 @@ require_relative main
 
 RSpec.describe Main do
   describe "#initialize" do
-    it "without any parameters" do
-      
+    it "running go-cli without any parameters" do
+      expect
+    end
+    it "running go-cli and passing three parameters" do
+
+    end
+    it "passing a filename as go-cli parameter" do
+
     end
   end
 end
