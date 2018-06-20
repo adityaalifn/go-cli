36f6b2894ca7db8a40fefbdcc5b976612f197e46
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-06-20 14:18:28 +0700

    remove Singleton design pattern from Application class and change it to Order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,9 @@
+require 'singleton'
+
 class Order
+  include Singleton
   UNIT_COST = 300
 
   def initialize
-
   end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
 require_relative 'lib/application'
 
-Application.instance.run
+app = Application.new
+app.run
