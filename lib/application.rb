98e2d4128af0069b44a6915845d3b6b83b25bf69
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-06-21 07:17:55 +0700

    parse input destination using map!

--- lib/application.rb
+++ lib/application.rb
@@ -7,7 +7,7 @@ require_relative 'order'
 class Application
   attr_reader :maps, :drivers, :user
 
-  def initialize(map_size=20, user_x_coordinate=Random.rand(20+1), user_y_coordinate=Random.rand(20+1))
+  def initialize(map_size=20, user_x_coordinate=Random.rand(20), user_y_coordinate=Random.rand(20))
     @maps = Maps.new(map_size)
 
     # initialize random driver position and user position
@@ -43,13 +43,14 @@ class Application
       @maps.show_map(@user, @drivers)
     when "2"
       print "Input your destination (x and y split by space): "
-      coordinate = gets.chomp.split(" ").map! {|i| i.to_i}
-      while coordinate.length != 2 or @maps.is_map_size_valid?(coordinate[0], coordinate[1]) or (coordinate[0] <= 0 or coordinate[1] <= 0)
+      coordinate = gets.chomp.split(" ").map {|i| i.to_i}
+      while coordinate.length != 2 or @maps.is_map_size_valid?(coordinate[0], coordinate[1]) or (coordinate[0] <= 0 or coordinate[1].to_i <= 0)
         puts "Invalid input." if coordinate.length != 2
-        puts "Destination out of range." if @maps.is_map_size_valid?(coordinate[0], coordinate[1])
-        puts "Destination must be number greater than zero." if coordinate[0] <= 0 or coordinate[1] <= 0
+        puts "Destination out of range." if @maps.is_map_size_valid?(coordinate[0], coordinate[1].to_i)
+        puts "Destination must be two number greater than zero." if coordinate[0] <= 0 or coordinate[1].to_i <= 0
+
         print "Input your destination (x and y split by space): "
-        coordinate = gets.chomp.split(" ")
+        coordinate = gets.chomp.split(" ").map {|i| i.to_i}
       end
       order = Order.instance
       order.order_go_ride(@drivers, @user, coordinate[0], coordinate[1], @maps)
