207a044587b6332dba38bd93db686546ff1d4c01
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-06-21 10:21:09 +0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,14 +2,17 @@ require_relative 'lib/application'
 
 input_array = ARGV
 
-if input_array.length <= 0
+if input_array.length == 0
   app = Application.new if input_array.length <= 0
-else
+elsif input_array.length == 1
+elsif input_array.length == 3
   begin
     app = Application.new(input_array[0].to_i, input_array[1].to_i, input_array[2].to_i)
   rescue
-    abort("Map size and coordinate must number greater than zero")
+    abort("Map size and coordinate must number greater than zero.")
   end
+else
+  abort ("Invalid number of argument.")
 end
 
 app.run
