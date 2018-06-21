e6c3dedb39f0f0d04b0113de6462964e498c0f44
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-06-21 18:09:53 +0700

    add spec for input command with file

--- main.rb
+++ main.rb
@@ -2,9 +2,7 @@ require_relative 'lib/application'
 
 input_array = ARGV
 
-if input_array.length == 0
-  app = Application.new if input_array.length <= 0
-elsif input_array.length == 1
+def parse_command_from_file(input_array)
   f = File.open(input_array[0], "r")
   command_list = f.read.split("\n")
   command = command_list.shift
@@ -16,7 +14,7 @@ elsif input_array.length == 1
       if map_size.to_i <= 0 or command.split[1].to_i <= 0 or command.split[2].to_i <= 0
         abort("Map size and coordinate must be number greater than zero.")
       else
-      app = Application.new(map_size.to_i, command.split[1].to_i, command.split[2].to_i)
+        app = Application.new(map_size.to_i, command.split[1].to_i, command.split[2].to_i)
       end
     end
   end
@@ -24,11 +22,17 @@ elsif input_array.length == 1
     driver = line.split
     abort("Invalid command input.") if driver[0] != "driver"
     abort("Driver coordinate must be number greater than zero.") if driver[2].to_i <= 0 and driver[3].to_i <= 0
-    puts driver[1]
     app.drivers << Driver.new(driver[2].to_i, driver[3].to_i, driver[1])
     app.maps.update_map(driver[2].to_i, driver[3].to_i, "D")
   end
   f.close
+  app
+end
+
+if input_array.length == 0
+  app = Application.new if input_array.length <= 0
+elsif input_array.length == 1
+  app = parse_command_from_file(input_array)
 elsif input_array.length == 3
   if input_array[0].to_i <= 0 or input_array[1].to_i <= 0 or input_array[2].to_i <= 0
     abort("Map size and coordinate must be number greater than zero.")
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,21 +1,48 @@
 require '../lib/application'
+require '../lib/maps'
 require 'rspec'
 
 RSpec.describe Application do
   describe "#initialize" do
     it "running go-cli without any parameters" do
       app = Application.new
-      expect(app.drivers.length).to eq(5)
-      expect(app.maps.size).to eq(20)
+      expect(Maps.size).to eq(20)
     end
     it "running go-cli and passing three parameters" do
       app = Application.new(25, 1,2)
-      expect(app.drivers.length).to eq(5)
-      expect(app.maps.size).to eq(25)
+      expect(Maps.size).to eq(25)
       expect(app.user.x_coordinate).to eq(1)
       expect(app.user.y_coordinate).to eq(2)
     end
     it "passing a filename as go-cli parameter" do
+      f = File.open("../command.txt", "r")
+      command_list = f.read.split("\n")
+      command = command_list.shift
+      if command.split[0] == "create_map"
+        map_size = command.split[1]
+
+        command = command_list.shift
+        if command.split[0] == "user_coordinate"
+          if map_size.to_i <= 0 or command.split[1].to_i <= 0 or command.split[2].to_i <= 0
+            abort("Map size and coordinate must be number greater than zero.")
+          else
+            app = Application.new(map_size.to_i, command.split[1].to_i, command.split[2].to_i)
+          end
+        end
+      end
+      command_list.each do |line|
+        driver = line.split
+        abort("Invalid command input.") if driver[0] != "driver"
+        abort("Driver coordinate must be number greater than zero.") if driver[2].to_i <= 0 and driver[3].to_i <= 0
+        app.drivers << Driver.new(driver[2].to_i, driver[3].to_i, driver[1])
+        app.maps.update_map(driver[2].to_i, driver[3].to_i, "D")
+      end
+      f.close
+
+      expect(Maps.size).to eq(20)
+      expect(app.user.x_coordinate).to eq(13)
+      expect(app.user.y_coordinate).to eq(13)
+      expect(Driver.total).to eq(4)
     end
   end
 end
